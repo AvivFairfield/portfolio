312c23c244cb94497ee4e321c142b1f220a0393f
AvivFairfield <avivcohen22@icloud.com>
2024-05-22 15:41:19 +0300

    fixed button

--- data/index.ts
+++ data/index.ts
@@ -96,27 +96,27 @@ export const projects = [
 
 export const testimonials = [
 	{
-		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aviv's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aviv is the ideal partner.",
 		name: "Michael Johnson",
 		title: "Director of AlphaStream Technologies",
 	},
 	{
-		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aviv's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aviv is the ideal partner.",
 		name: "Michael Johnson",
 		title: "Director of AlphaStream Technologies",
 	},
 	{
-		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aviv's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aviv is the ideal partner.",
 		name: "Michael Johnson",
 		title: "Director of AlphaStream Technologies",
 	},
 	{
-		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aviv's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aviv is the ideal partner.",
 		name: "Michael Johnson",
 		title: "Director of AlphaStream Technologies",
 	},
 	{
-		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+		quote: "Collaborating with Aviv was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Aviv's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Aviv is the ideal partner.",
 		name: "Michael Johnson",
 		title: "Director of AlphaStream Technologies",
 	},
